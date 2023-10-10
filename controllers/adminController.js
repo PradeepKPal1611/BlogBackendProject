752653b6f57ed3b46737022e464626f1640e5b5d
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-10-10 17:21:25 +0530

    BlogSetup code, Layout code, AdminControllerUpdated Code

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,23 +1,85 @@
-const blogOne = async(req, res)=>{
+const BlogSetting = require('../models/blogSettingModel');
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
-    res.send('Blog 1');
+const securePassword = async (password) => {
+
+    try {
+
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        return passwordHash;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const login = async (req, res) => {
+
+    res.send('Hey Please Login here!')
 
 }
 
-const blogTwo = async(req, res)=>{
 
-    res.send('Blog 2');
+const blogSetup = async (req, res) => {
+
+    try {
+
+        var blogSetting = await BlogSetting.find({});
+
+        if (blogSetting.length > 0) {
+            res.redirect('/login');
+        }
+        else {
+            res.render('blogSetup');
+        }
+
+    } catch (error) {
+        console.log(error.message);
+    }
 
 }
 
-const blogSetup = async(req, res)=>{
+const blogSetupSave = async(req, res) =>{
 
-    res.send('New Blog Setup here');
+    try {
 
+        const blog_title = req.body.blog_title;
+        const blog_image = req.file.filename;
+        const Description = req.body.Description;
+        const email = req.body.email;
+        const name = req.body.name;
+        const password = await securePassword(req.body.password);
+
+        const blogSetting = new BlogSetting({
+            blog_title: blog_title,
+            blog_logo: blog_image,
+            description: Description
+        });
+
+        await blogSetting.save();
+        const user = new User({
+            name: name,
+            email: email,
+            password: password,
+            is_admin: 1
+        });
+
+        const userData = await user.save();
+        if (userData) {
+            res.redirect('/login');
+        }
+        else {
+            res.render('blogSetup', { message: 'Blog is not Setup Properly' });
+        }
+
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 module.exports = {
-    blogOne,
-    blogTwo,
-    blogSetup
+    login,
+    blogSetup,
+    blogSetupSave
 }
